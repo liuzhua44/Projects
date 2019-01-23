e06245d4bf5575491628ef68ac75543536009bfb
liuzhua44 <44562880+liuzhua44@users.noreply.github.com>
2019-01-23 17:32:04 +0800

    打印账号

--- electron-quick-start/renderer.js
+++ electron-quick-start/renderer.js
@@ -266,12 +266,12 @@ ipcRenderer.on("gameAccountOndragEnd", (event, info) => {
 
     }
     createImportAccount(info.tableJson, option);
+    createPrintAccount();
 })
 
+// 导入账号数据
+var importJson = new Array();
 createImportAccount = function (dataJson, option) {
-    // 导入账号数据
-    var importJson = new Array();
-
     // 账号，2位/3位
     var accountIndex = {
         "xx": 0,
@@ -377,6 +377,91 @@ createImportAccount = function (dataJson, option) {
         $("#gaImportTable").append(html);
     }
 
+}
+
+var printJson = new Array();
+createPrintAccount = function(){
+    for(var i=0;i<importJson.length;i++){
+        var info = importJson[i];
+        var row = new Array();
+        row.push(info[5]);
+        row.push(info[2]);
+        row.push(info[4]);
+        row.push("竞赛账号:");
+        row.push(info[0]);
+        row.push("密码:");
+        row.push(info[1]);
+        printJson.push(row)
+    }
+    for(var i=0;i<printJson.length;i++){
+        var html = '<tr>';
+        for(var j=0;j<printJson[i].length;j++){
+            html += '<td>' + printJson[i][j] + '</td>';
+        }
+        html += '</tr>';
+        $("#gaPrintTable").append(html);
+    }
+}
+
+/**
+ * 通用的打开下载对话框方法，没有测试过具体兼容性
+ * @param url 下载地址，也可以是一个blob对象，必选
+ * @param saveName 保存文件名，可选
+ */
+function openDownloadDialog(url, saveName)
+{
+    if(typeof url == 'object' && url instanceof Blob)
+    {
+        url = URL.createObjectURL(url); // 创建blob地址
+    }
+    var aLink = document.createElement('a');
+    aLink.href = url;
+    aLink.download = saveName || ''; // HTML5新增的属性，指定保存文件名，可以不要后缀，注意，file:///模式下不会生效
+    var event;
+    if(window.MouseEvent) event = new MouseEvent('click');
+    else
+    {
+        event = document.createEvent('MouseEvents');
+        event.initMouseEvent('click', true, false, window, 0, 0, 0, 0, 0, false, false, false, false, 0, null);
+    }
+    aLink.dispatchEvent(event);
+}
+
+// 将一个sheet转成最终的excel文件的blob对象，然后利用URL.createObjectURL下载
+function sheet2blob(sheet, sheetName) {
+    sheetName = sheetName || 'sheet1';
+    var workbook = {
+        SheetNames: [sheetName],
+        Sheets: {}
+    };
+    workbook.Sheets[sheetName] = sheet;
+    // 生成excel的配置项
+    var wopts = {
+        bookType: 'xlsx', // 要生成的文件类型
+        bookSST: false, // 是否生成Shared String Table，官方解释是，如果开启生成速度会下降，但在低版本IOS设备上有更好的兼容性
+        type: 'binary'
+    };
+    var wbout = XLSX.write(workbook, wopts);
+    var blob = new Blob([s2ab(wbout)], {type:"application/octet-stream"});
+    // 字符串转ArrayBuffer
+    function s2ab(s) {
+        var buf = new ArrayBuffer(s.length);
+        var view = new Uint8Array(buf);
+        for (var i=0; i!=s.length; ++i) view[i] = s.charCodeAt(i) & 0xFF;
+        return buf;
+    }
+    return blob;
+}
+
+saveImportExcle = function(){
+    console.log("saveImportExcle")
+    var sheet = XLSX.utils.aoa_to_sheet(importJson);
+    openDownloadDialog(sheet2blob(sheet),"导入账号.xlsx")
+}
+
+savePrintExcle = function(){
+    var sheet = XLSX.utils.aoa_to_sheet(printJson);
+    openDownloadDialog(sheet2blob(sheet),"打印账号.xlsx")
 }
 
 // fenxi = function () {
